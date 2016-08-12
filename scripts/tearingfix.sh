5b74b6d00eabb863dd55876d72ecd59879eb2fd3
Ken <ken@ken-pc.lan>
2016-08-12 09:38:00 +0200

    startup fix nvidia tearing

---
+++ scripts/tearingfix.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#profile file to supress tearing with nvidia drivers
+nvidia-settings --assign CurrentMetaMode="$(xrandr | sed -nr '/(\S+) connected (primary )?[0-9]+x[0-9]+(\+\S+).*/{ s//\1: nvidia-auto-select \3 { ForceFullCompositionPipeline = On }, /; H }; ${ g; s/\n//g; s/, $//; p }')"
+
