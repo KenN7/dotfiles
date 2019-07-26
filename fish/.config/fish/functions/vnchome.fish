a67da9eb465331d036ab5229d183cc70aeddd126
Ken <ken@kenh.fr>
2019-07-26 08:02:09 +0200

    tmux conf from 2.8.X to 2.9.X

--- fish/.config/fish/functions/vnchome.fish
+++ fish/.config/fish/functions/vnchome.fish
@@ -1,8 +1,8 @@
 ## function to connect to remote ssh, launch vnc server and connect client
 
 function vnchome
-    ssh ken@home.kenh.fr 'nohup x0vncserver -rfbport=5000 -display :0 -passwordfile ~/.vnc/passwd > /dev/null 2>&1 &'
+    ssh ken@10.10.10.11 'nohup x0vncserver -rfbport=5000 -display :0 -passwordfile ~/.vnc/passwd > /dev/null 2>&1 &'
     sleep 2
-    vncviewer DotWhenNoCursor=1 home.kenh.fr::8888
-    ssh ken@home.kenh.fr 'killall x0vncserver'; and echo 'x0vnc killed remotely'
+    vncviewer DotWhenNoCursor=1 10.10.10.11::8888
+    ssh ken@10.10.10.11 'killall x0vncserver'; and echo 'x0vnc killed remotely'
 end
