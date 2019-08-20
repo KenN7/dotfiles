4a306062393a5cbfc0ca6c2fbf4d1ec08f7c2200
Ken <ken@kenh.fr>
2019-08-20 18:25:43 +0200

    check executables for figlet, lolcat and keychain

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,14 @@
 . ~/.bash_aliases
 
 set -x EDITOR vim
-if status --is-interactive
+if status --is-interactive; and type -q keychain
   keychain --eval --quiet -Q id_rsa | source
 end
 
+if type -q thefuck
 eval (thefuck --alias | tr '
 ' ';')
+end
 
 set PATH /home/ken/dotfiles/scripts /home/ken/.local/bin $PATH
 source /home/ken/.config/fish/aliases.fish
--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,4 +1,12 @@
 function fish_greeting
-    hostname | figlet | lolcat -F 0.3
+    if type -q figlet; and type -q lolcat
+        hostname | figlet | lolcat -F 0.3
+    else if type -q figlet
+        hostname | figlet
+    else
+        echo ">>>>>>"
+        hostname
+        echo "<<<<<<"
+    end
     uptime
 end
