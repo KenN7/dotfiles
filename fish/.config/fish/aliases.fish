be57b83f3b9b04b9b2176862b5d1df509a58639f
Ken <ken@kenh.fr>
2021-09-24 15:26:55 +0200

    augmented fish capab

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -5,11 +5,71 @@ alias com="git commit"
 alias pus="git push"
 alias pul="git pull"
 alias cmakevim="cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-alias ls='ls --file-type --color=auto --time-style=+"%d.%m.%Y-%H:%M"'
-alias grep='grep --color=auto'
 alias cp='cp -r'
-alias vi='vim'
+alias vi='nvim'
+alias vim="nvim"
 alias vd='vimdiff'
 alias logall='git log --pretty=oneline --abbrev-commit --graph'
 alias restart_plasma='kquitapp5 plasmashell; and kstart5 plasmashell'
 alias fix_kwin='kwin_x11 --replace'
+
+
+## Useful aliases
+# Replace ls with exa
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+
+# Replace some more things with better alternatives
+alias cat='bat --style header --style rules --style snip --style changes --style header'
+[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
+
+# Common use
+alias grubup="sudo update-grub"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias rmpkg="sudo pacman -Rdd"
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias upd='/usr/bin/update'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'			# List amount of -git packages
+
+
+# Get fastest mirrors 
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist" 
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist" 
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist" 
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist" 
+
+# Help people new to Arch
+alias please='sudo'
+alias tb='nc termbin.com 9999'
+
+# Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# Recent installed packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+
+
+mcfly init fish | source
+
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,7 @@ if test -e $HOME/.config/fish/local.fish
     source $HOME/.config/fish/local.fish
 end
 
-set -x EDITOR vim
+set -x EDITOR nvim
 if status --is-interactive; and type -q keychain
   keychain --eval --quiet -Q id_rsa | source
 end
@@ -18,3 +18,43 @@ end
 set PATH $HOME/.cargo/bin $HOME/dotfiles/scripts $HOME/.local/bin $HOME/.npm/bin $HOME/.local/share/gem/ruby/3.0.0/bin $PATH
 set NODE_PATH $HOME/.npm/lib/node_modules $NODE_PATH
 source $HOME/.config/fish/aliases.fish
+
+## Starship prompt
+if status --is-interactive
+   # source ("/usr/bin/starship" init fish --print-full-init | psub)
+end
+
+## Functions
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+
