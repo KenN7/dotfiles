cf1d2ad1213a898f5b1366043e23d227d091be21
Ken <ken@kenh.fr>
2021-12-14 13:50:25 +0100

    up vncto, rm vnchome

--- fish/.config/fish/functions/vnchome.fish
+++
@@ -1,8 +0,0 @@
-## function to connect to remote ssh, launch vnc server and connect client
-
-function vnchome
-    ssh ken@10.10.10.11 'nohup x0vncserver -rfbport=5000 -display :0 -passwordfile ~/.vnc/passwd > /dev/null 2>&1 &'
-    sleep 2
-    vncviewer DotWhenNoCursor=1 10.10.10.11::8888
-    ssh ken@10.10.10.11 'killall x0vncserver'; and echo 'x0vnc killed remotely'
-end
--- fish/.config/fish/functions/vncto.fish
+++ fish/.config/fish/functions/vncto.fish
@@ -1,8 +1,11 @@
 ## function to connect to remote ssh, launch vnc server and connect client
 
+## usage: vncto $user $ip $port, target has to have x0vncserver and client tigervnc
+## e.g. vncto ken 192.168.1.50 22
+
 function vncto
     ssh $argv[1]@$argv[2] -p $argv[3] 'nohup x0vncserver -rfbport=5000 -display :0 -passwordfile ~/.vnc/passwd > /dev/null 2>&1 &'
     sleep 2
-    vncviewer DotWhenNoCursor=1 argv[2]::5000
+    vncviewer DotWhenNoCursor=1 $argv[2]::5000
     ssh $argv[1]@$argv[2] -p $argv[3] 'killall x0vncserver'; and echo 'x0vnc killed remotely'
 end
